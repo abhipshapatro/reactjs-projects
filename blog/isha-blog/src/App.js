74dacd38c9dba9ddb6143413ca50d9b3d8fd866e
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-02-10 16:07:17 +0530

    dynamic values in templates

--- blog/isha-blog/src/App.js
+++ blog/isha-blog/src/App.js
@@ -2,10 +2,27 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // const person = {name : 'isha' , age: 21}; //doesnot take objects/arrays
+  const link = "http://www.google.com";
+
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+
+        {/* <p>{person}</p> */}
+
+        <p>{ 10 }</p>
+        <p> {"hello guys"} </p>
+        <p>{1,2,3,4,5}</p>
+        <p>{Math.random()*10}</p>
+
+
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
