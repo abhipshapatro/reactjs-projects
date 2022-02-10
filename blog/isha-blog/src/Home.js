013861b3c958a1c9e5ba6c46846916bae8131c03
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-02-10 17:40:23 +0530

    click events

--- blog/isha-blog/src/Home.js
+++ blog/isha-blog/src/Home.js
@@ -1,9 +1,25 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("hello peeps", e);
+    }
+
+    const handleClickAgain = (name , e) => {
+        console.log('hello ' + name , e.target);
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) =>handleClickAgain('isha' , e)}>Click me again</button>
         </div>
     );
 }
  
 export default Home;
+
+// handleClick() will invoke the function direcly
+// so we write just the function name
+// handleClickAgain() cant be invoked directly (reason above)
+// as it is wrapped in an anonymous function which works only when the user clicks on the button 
