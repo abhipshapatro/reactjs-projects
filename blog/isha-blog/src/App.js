1e173c9dcf736a14a64746f3895701d181b74874
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-02-18 01:09:56 +0530

    route parameters

--- blog/isha-blog/src/App.js
+++ blog/isha-blog/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
           <Route path="/create">
             <Create />
           </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails />
+          </Route>
         </Switch>
       </div>
     </div>
@@ -26,3 +30,4 @@ function App() {
 export default App;
 
 //switch surrounds the route >> makes sure only one route component shows at one time
+// id = name of the route parameter
