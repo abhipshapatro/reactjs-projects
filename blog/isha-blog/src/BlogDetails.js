6de210b744a7beeef064241b0dc67b262e923e11
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-02-18 20:07:59 +0530

    reusing custom hooks

--- blog/isha-blog/src/BlogDetails.js
+++ blog/isha-blog/src/BlogDetails.js
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./usefetch";
 
 const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
 
-    const { id } = useParams();
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written By {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
+    </div>
+  );
+};
 
-    return ( 
-        <div className="blog-details">
-            <h2>Blog details - { id }</h2>
-        </div>
-     );
-}
- 
 export default BlogDetails;
